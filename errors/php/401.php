3beb2f0712043d447237a32a047827d5a901bed9
Beard <beardlyness@users.noreply.github.com>
2020-04-25 09:31:39 -0700

    Update 401.php

--- errors/php/401.php
+++ errors/php/401.php
@@ -2,7 +2,7 @@
   <body>
     <section>
       <a href="https://github.com/beardlyness/NGINXY"><h1 class="maintxt">NGINXY</h1></a>
-      <a href="https://nginx.org/en/docs/"><img class="logo" src="https://camo.githubusercontent.com/246d2b6c9f17db8aebb5b57b938229877433f2f3/68747470733a2f2f63646e2d312e77702e6e67696e782e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031382f30332f69636f6e2d4e47494e582d4f53532e737667" /></a>
+      <a href="https://nginx.org/en/docs/"><img class="logo" src="https://raw.githubusercontent.com/beardlyness/NGINXY-Custom-Errors/master/nginxy.png" /></a>
 
         <p><code><a href="https://httpstatuses.com/401">Error 401 Unauthorized</a></code></p>
         <p>This is the 401 Error Page for NGINXY.</p>
